cb0d9153fe5d74c656352c43f58c870b4ec99344
Aleksandar Josic <aleksa_josic@hotmail.com>
2022-01-13 16:56:30 -0800

    blurb for stopping vids on modal closes

--- js/freelancer.js
+++ js/freelancer.js
@@ -26,6 +26,15 @@ $(function() {
     });
 });
 
+$(function(){
+    $("body").on('hidden.bs.modal', function (e) {
+      var $iframes = $(e.target).find("iframe");
+      $iframes.each(function(index, iframe){
+        $(iframe).attr("src", $(iframe).attr("src"));
+      });
+    });
+  });
+
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
     target: '.navbar-fixed-top'
